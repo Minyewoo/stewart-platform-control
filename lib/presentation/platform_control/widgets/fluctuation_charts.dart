c45381a3a91bb3fec36f42ecac5da9c6dfaca933
Minyewoo <const.trushov@gmail.com>
2024-12-17 11:23:56 +0300

    FluctuationCharts | Added period and z-velocity charts

--- lib/presentation/platform_control/widgets/fluctuation_charts.dart
+++ lib/presentation/platform_control/widgets/fluctuation_charts.dart
@@ -28,7 +28,6 @@ class _FluctuationChartsState extends State<FluctuationCharts> {
   //
   @override
   void initState() {
-    final isoNow = DateTime.now().toIso8601String();
     final dsClient = widget._dsClient;
     _pointLogger = CsvDataPointLogger(
       pointsStreams: [
@@ -39,10 +38,12 @@ class _FluctuationChartsState extends State<FluctuationCharts> {
         dsClient.streamReal('Platform.DeltaAngleX'),
         dsClient.streamReal('Platform.DeltaAngleY'),
         dsClient.streamReal('Platform.DeltaAngleZ'),
-        dsClient.streamReal('Platform.Heave')
+        dsClient.streamReal('Platform.Heave'),
+        dsClient.streamReal('Platform.FluctuationPeriod'),
+        dsClient.streamReal('Platform.VelocityZ'),
       ],
-      directoryPath: join('run_logs', isoNow),
-      converter: ListToCsvConverter(
+      directoryPath: join('run_logs', DateTime.now().toIso8601String()),
+      converter: const ListToCsvConverter(
         fieldDelimiter: ';',
         eol: '\r\n',
       ),
@@ -60,6 +61,9 @@ class _FluctuationChartsState extends State<FluctuationCharts> {
   Widget build(BuildContext context) {
     const liveAxisBufferLength = 12000;
     const xMarkInterval = Duration(seconds: 6);
+    final xMarkIntervalMilliseconds = xMarkInterval.inMilliseconds.toDouble();
+    final minX = DateTime.now().subtract(xMarkInterval)
+      .millisecondsSinceEpoch.toDouble();
     return Column(
       children: [
         Expanded(
@@ -69,9 +73,8 @@ class _FluctuationChartsState extends State<FluctuationCharts> {
                 child: Card(
                   child: LiveChartWidget(
                     legendWidth: 220,
-                    minX: DateTime.now().subtract(xMarkInterval)
-                      .millisecondsSinceEpoch.toDouble(),
-                    xInterval: xMarkInterval.inMilliseconds.toDouble(),
+                    minX: minX,
+                    xInterval: xMarkIntervalMilliseconds,
                     axes: [
                       LiveAxis(
                         bufferLength: liveAxisBufferLength,
@@ -102,22 +105,21 @@ class _FluctuationChartsState extends State<FluctuationCharts> {
                 child: Card(
                   child: LiveChartWidget(
                     legendWidth: 270,
-                    minX: DateTime.now().subtract(xMarkInterval)
-                      .millisecondsSinceEpoch.toDouble(),
-                    xInterval: xMarkInterval.inMilliseconds.toDouble(),
+                    minX: minX,
+                    xInterval: xMarkIntervalMilliseconds,
                     axes: [
                       LiveAxis(
                         bufferLength: liveAxisBufferLength,
                         stream: widget._platformStateStream,
                         signalName: 'phiX',
-                        caption: 'Задание угла вокруг Y, градусы',
+                        caption: 'Задание угла вокруг Y, град',
                         color: Colors.redAccent,
                       ),
                       LiveAxis(
                         bufferLength: liveAxisBufferLength,
                         stream: widget._platformStateStream,
                         signalName: 'phiY',
-                        caption: 'Задание угла вокруг X, градусы',
+                        caption: 'Задание угла вокруг X, град',
                         color: Colors.blueAccent,
                       ),
                     ],
@@ -134,29 +136,28 @@ class _FluctuationChartsState extends State<FluctuationCharts> {
                 child: Card(
                   child: LiveChartWidget(
                     legendWidth: 260,
-                    minX: DateTime.now().subtract(xMarkInterval)
-                      .millisecondsSinceEpoch.toDouble(),
-                    xInterval: xMarkInterval.inMilliseconds.toDouble(),
+                    minX: minX,
+                    xInterval: xMarkIntervalMilliseconds,
                     axes: [
                       LiveAxis(
                         bufferLength: liveAxisBufferLength,
                         stream: widget._dsClient.streamReal('Platform.Roll'),
                         signalName: 'Platform.Roll',
-                        caption: 'Угол крена (roll), градусы',
+                        caption: 'Угол крена (roll), град',
                         color: Colors.purpleAccent,
                       ),
                       LiveAxis(
                         bufferLength: liveAxisBufferLength,
                         stream: widget._dsClient.streamReal('Platform.Pitch'),
                         signalName: 'Platform.Pitch',
-                        caption: 'Угол тангажа (pitch), градусы',
+                        caption: 'Угол тангажа (pitch), град',
                         color: Colors.greenAccent,
                       ),
                       // LiveAxis(
                       //   bufferLength: liveAxisBufferLength,
                       //   stream: _dsClient.streamReal('Platform.Yaw'),
                       //   signalName: 'Platform.Yaw',
-                      //   caption: 'Угол рыскания (yaw), градусы',
+                      //   caption: 'Угол рыскания (yaw), град',
                       //   color: Colors.orangeAccent,
                       // ),
                     ],
@@ -167,22 +168,21 @@ class _FluctuationChartsState extends State<FluctuationCharts> {
                 child: Card(
                   child: LiveChartWidget(
                     legendWidth: 280,
-                    minX: DateTime.now().subtract(xMarkInterval)
-                      .millisecondsSinceEpoch.toDouble(),
-                    xInterval: xMarkInterval.inMilliseconds.toDouble(),
+                    minX: minX,
+                    xInterval: xMarkIntervalMilliseconds,
                     axes: [
                       LiveAxis(
                         bufferLength: liveAxisBufferLength,
                         stream: widget._dsClient.streamReal('Platform.DeltaAngleX'),
                         signalName: 'Platform.DeltaAngleX',
-                        caption: 'Угловая скорость X, градусы/с - 1',
+                        caption: 'Угловая скорость X, град/с - 1',
                         color: Colors.purpleAccent,
                       ),
                       LiveAxis(
                         bufferLength: liveAxisBufferLength,
                         stream: widget._dsClient.streamReal('Platform.DeltaAngleY'),
                         signalName: 'Platform.DeltaAngleY',
-                        caption: 'Угловая скорость Y, градусы/с - 1',
+                        caption: 'Угловая скорость Y, град/с - 1',
                         color: Colors.greenAccent,
                       ),
                       // LiveAxis(
@@ -205,15 +205,54 @@ class _FluctuationChartsState extends State<FluctuationCharts> {
               Expanded(
                 child: Card(
                   child: LiveChartWidget(
-                    minX: DateTime.now().subtract(xMarkInterval)
-                      .millisecondsSinceEpoch.toDouble(),
-                    xInterval: xMarkInterval.inMilliseconds.toDouble(),
+                    minX: minX,
+                    xInterval: xMarkIntervalMilliseconds,
                     axes: [
                       LiveAxis(
                         bufferLength: liveAxisBufferLength,
                         stream: widget._dsClient.streamReal('Platform.Heave'),
                         signalName: 'Platform.Heave',
-                        caption: 'Heave, метры',
+                        caption: 'Heave, м',
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Card(
+                  child: LiveChartWidget(
+                    minX: minX,
+                    xInterval: xMarkIntervalMilliseconds,
+                    axes: [
+                      LiveAxis(
+                        bufferLength: liveAxisBufferLength,
+                        stream: widget._dsClient.streamReal('Platform.FluctuationPeriod'),
+                        signalName: 'Platform.FluctuationPeriod',
+                        caption: 'Период качки, с',
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Card(
+                  child: LiveChartWidget(
+                    minX: minX,
+                    xInterval: xMarkIntervalMilliseconds,
+                    axes: [
+                      LiveAxis(
+                        bufferLength: liveAxisBufferLength,
+                        stream: widget._dsClient.streamReal('Platform.VelocityZ'),
+                        signalName: 'Platform.VelocityZ',
+                        caption: 'Скорость по Z, м/с',
                         color: Colors.blue,
                       ),
                     ],
